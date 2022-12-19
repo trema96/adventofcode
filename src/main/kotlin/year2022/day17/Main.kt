2405f3d5ec3abea82414008e503a2f784d2e88e3
Luca Tremamunno <luca@icure.com>
2022-12-19 22:00:37 +0100

    Day 17

--- src/main/kotlin/year2022/day17/Main.kt
+++ src/main/kotlin/year2022/day17/Main.kt
@@ -2,7 +2,7 @@ package year2022.day17
 
 import java.io.File
 
-data class Point(val x: Int, val y: Int)
+data class Point(val x: Long, val y: Long)
 
 data class Rock(val points: List<Point>) {
     fun moveLeft()  = Rock(points.map { it.copy(x = it.x - 1) })
@@ -18,9 +18,10 @@ data class Rock(val points: List<Point>) {
 }
 
 class RockGenerator {
-    private var index = 0
+    var index = 0
+        private set
 
-    fun nextRock(highestPoint: Int): Rock {
+    fun nextRock(highestPoint: Long): Rock {
         val y = highestPoint + 4
         val rock = when (index % 5) {
             0 -> Rock(listOf(
@@ -66,11 +67,12 @@ class Volcano(private val gusts: List<Char>) {
     val fallenRocks = mutableSetOf<Point>()
     private val generator = RockGenerator()
     private var gustIndex = 0
-    var highestPoint = -1
+    var highestPoint = -1L
+        private set
 
-    fun simulateRock() {
-        var currRock: Rock? = generator.nextRock(highestPoint)
-        while (currRock != null) {
+    fun simulateRock(): VolcanicHistory {
+        var currRock: Rock = generator.nextRock(highestPoint)
+        while (true) {
             val sideMovementCandidate = if (nextGust() == '<') currRock.moveLeft() else currRock.moveRight()
             if (sideMovementCandidate.inBounds && !sideMovementCandidate.collidesWithAnyOf(fallenRocks)) {
                 currRock = sideMovementCandidate
@@ -79,8 +81,9 @@ class Volcano(private val gusts: List<Char>) {
             if (downMovementCandidate.points.any { it.y < 0 } || downMovementCandidate.collidesWithAnyOf(fallenRocks)) {
                 fallenRocks += currRock.points
                 val highestOfRock = currRock.points.maxOf { it.y }
+                val prevHighest = highestPoint
                 if (highestOfRock > highestPoint) highestPoint = highestOfRock
-                currRock = null
+                return VolcanicHistory(generator.index, gustIndex, highestPoint - prevHighest)
             } else {
                 currRock = downMovementCandidate
             }
@@ -90,20 +93,39 @@ class Volcano(private val gusts: List<Char>) {
     fun nextGust(): Char = gusts[gustIndex].also { gustIndex = (gustIndex + 1) % gusts.size  }
 }
 
+data class VolcanicHistory(
+    val rockState: Int,
+    val gustState: Int,
+    val dh: Long
+) {
+    val state = rockState to gustState
+}
+
 fun main() {
     val gusts = File("./data/2022/input_17.txt").readText().toList().filter { it == '<' || it == '>' }
     val volcano = Volcano(gusts)
-//    repeat(5) {
-//        volcano.simulateRock()
-//        val maxY = volcano.fallenRocks.maxOf { it.y }
-//        (0 .. maxY).reversed().forEach { y ->
-//            (0 .. 6).forEach { x ->
-//                if (Point(x, y) in volcano.fallenRocks) print("#") else print(".")
-//            }
-//            println()
-//        }
-//        println(volcano.highestPoint)
-//    }
     repeat(2022) { volcano.simulateRock() }
     println(volcano.highestPoint + 1)
+    val volcano2 = Volcano(gusts)
+    val repetitionSearchSize = 100_000
+    val historySample = (0 until repetitionSearchSize).map { volcano2.simulateRock() }
+    val repetitionState = historySample.last().state
+    val indicesOfRepetition = historySample.withIndex().filter { it.value.state == repetitionState }.map { it.index }
+    fun last2SlicesOfSize(size: Int): Pair<List<VolcanicHistory>, List<VolcanicHistory>> {
+        val reversed = indicesOfRepetition.reversed()
+        val secondSliceStart = reversed[size * 2]
+        val secondSliceEnd = reversed[size]
+        return Pair(
+            historySample.slice((secondSliceEnd + 1)..reversed[0]),
+            historySample.slice((secondSliceStart + 1)..secondSliceEnd)
+        )
+    }
+    val sizeOfSlice = (1 until Int.MAX_VALUE).first { last2SlicesOfSize(it).let { (a, b) -> a == b }}
+    val repeatingHistory = last2SlicesOfSize(sizeOfSlice).first
+    val fullRepeatingHistoryDelta = repeatingHistory.sumOf { it.dh }
+    val target = 1_000_000_000_000
+    val remaining = target - historySample.size
+    val fullRepetitionsDelta = (remaining / repeatingHistory.size) * fullRepeatingHistoryDelta
+    val leftoverRepetitionDelta = repeatingHistory.take((remaining % repeatingHistory.size).toInt()).sumOf { it.dh }
+    println(volcano2.highestPoint + fullRepetitionsDelta + leftoverRepetitionDelta + 1)
 }
